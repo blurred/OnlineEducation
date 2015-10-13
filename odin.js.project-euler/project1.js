f488092ccc81912901d58acd3958eb091114d924
blurred <jasonseril@gmail.com>
2015-10-13 23:33:27 +0900

    new code

--- odin.js.project-euler/project1.js
+++ odin.js.project-euler/project1.js
@@ -2,7 +2,7 @@
  * Created by jason on 2015-10-13.
  */
 
-var solution = function(input){
+var multiples = function multiples(input) {
     var sum = 0;
     for(var i = 1; i < input; i++){
         if(i % 3 === 0){
@@ -12,5 +12,8 @@ var solution = function(input){
             sum += i;
         }
     }
+    //console.log(sum);
     return sum;
 };
+
+var results = document.write(multiples(10));
